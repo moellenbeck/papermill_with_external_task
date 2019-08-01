972bda9c3ddc83f05529f8e20d456077f9363951
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2019-08-01 06:59:14 +0200

    Simplify handling of external tasks

--- external_worker.py
+++ external_worker.py
@@ -2,74 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import json
 
-import asyncio
 import papermill as pm
 
-from external_task_api_client.external_task_api_client_service import ExternalTaskApiClientService
-from external_task_api_client.external_task_worker import ExternalTaskWorker
+from process_engine_client import ProcessEngineClient, BpmnError
 
-from external_task_api_client.external_task_api_client_service import ExternalTaskApiClientService
-from external_task_api_client.external_task_worker import ExternalTaskWorker
+def _worker(payload):
 
+    input_filename = 'input_papermill.ipynb'
+    output_filename = 'output_papermill.ipynb'
 
-from external_task_api_client.external_task_results.finished import ExternalTaskFinished
-from external_task_api_client.external_task_results.bpmn_error import ExternalTaskBpmnError
+    hello_param = payload['a key']
 
-@asyncio.coroutine
-def _start_worker(worker, identity, topic, handle_action):
-    return worker.wait_for_handle(
-        identity=identity,
-        topic=topic,
-        max_tasks=10,
-        long_polling_timeout=10_000,
-        handle_action=handle_action
+    pm.execute_notebook(
+        input_filename,
+        output_filename,
+        parameters=dict(hello=hello_param)
     )
 
-async def _handle_work(task):
-    try:
-        print(json.dumps(task, sort_keys=True, indent=2))
+    result = {"output_filename": output_filename}
 
-        input_filename = 'input_papermill.ipynb'
-        output_filename = 'output_papermill.ipynb'
+    #raise BpmnError(300, 'bpmn fehler - auch provoziert')
+    #raise Exception('provozierter unspezifischer Fehler')
 
-        hello_param = task['payload']['a key']
-        #hello_param = task.payload['a key'] # with error
-
-        pm.execute_notebook(
-            input_filename,
-            output_filename,
-            parameters=dict(hello=hello_param)
-        )
-
-        task_result = {"output_filename": output_filename}
-
-        return ExternalTaskFinished(task["id"], task_result)
-    except Exception as e:
-        print('Raised a exception: ', e)
-        return ExternalTaskBpmnError(task["id"], "error_message", {'hello': 'world'})
-
-def handle_external_task(process_engine_url, identity, topic, worker_func):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(None)
-
-    print('Using ProcessEngine at "{}"'.format(process_engine_url))
-    external_task_client = ExternalTaskApiClientService(process_engine_url)
-
-    worker = ExternalTaskWorker(external_task_client)
-
-    loop_runner = _start_worker(worker, identity, topic, worker_func)
-
-    loop.run_until_complete(loop_runner)
+    return result
 
 def main():
     process_engine_location = sys.argv[1] if len(sys.argv) == 2 else 'http://localhost:8000'
-
-    identity = {"token": "ZHVtbXlfdG9rZW4="}
-
     topic = 'PapermillTopic'
 
-    handle_external_task(process_engine_location, identity, topic, _handle_work)
+    process_engine_client = ProcessEngineClient(process_engine_location)
+
+    process_engine_client.subscribe_to_external_tasks_with_topic(topic, _worker)
 
 main()
---
+++ process_engine_client.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio
+import json
+
+from external_task_api_client.external_task_api_client_service import ExternalTaskApiClientService
+from external_task_api_client.external_task_worker import ExternalTaskWorker
+
+from external_task_api_client.external_task_results.finished import ExternalTaskFinished
+from external_task_api_client.external_task_results.bpmn_error import ExternalTaskBpmnError
+
+class BpmnError(Exception):
+
+    def __init__(self, error_code, error_message):
+        self.error_code = error_code
+        self.error_message = error_message
+
+class ProcessEngineClient:
+
+    def __init__(self, process_engine_url, identity=None):
+        self._process_engine_url = process_engine_url
+
+        if identity is None:
+            self._identity = {"token": "ZHVtbXlfdG9rZW4="}
+        else:
+            self._identity = identity
+
+        self._loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
+
+        print('Using ProcessEngine at "{}"'.format(self._process_engine_url))
+        external_task_client = ExternalTaskApiClientService(self._process_engine_url)
+
+        self._worker = ExternalTaskWorker(external_task_client)
+
+    @asyncio.coroutine
+    def _start_external_task(self, topic, handle_action, options={}):
+        max_tasks = options.get('max_tasks', 10)
+        long_polling_timeout = options.get('long_polling_timeout', 10_000)
+        dump_task = options.get('dump_task', True)
+
+        async def wrapper_handle_action(task):
+            print(json.dumps(task, sort_keys=True, indent=2)) is dump_task
+
+            payload = task['payload']
+            task_id = task['id']
+
+            try:
+                result = handle_action(payload)
+
+                return ExternalTaskFinished(task_id, result)
+            except BpmnError as be:
+                print('bpmn error raised: ', be)
+                return ExternalTaskBpmnError(task_id, be.error_code, {'message': be.error_message})
+            except Exception as e:
+                print('Unspecified exception raised: ', e)
+                return ExternalTaskBpmnError(task_id, "undefined", {'message': str(e)})
+
+        return self._worker.wait_for_handle(
+            identity=self._identity,
+            topic=topic,
+            max_tasks=max_tasks,
+            long_polling_timeout=long_polling_timeout,
+            handle_action=wrapper_handle_action
+        )
+
+    def subscribe_to_external_tasks_with_topic(self, topic, worker_func):
+        loop_runner = self._start_external_task(topic, worker_func)
+
+        self._loop.run_until_complete(loop_runner)
